e97d7866e5d493240c32d837aba30d5a0bb79492
George Goldberg <george@appletreetechnology.com>
2022-10-16 20:57:23 +0100

    Add CORS middleware.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import datetime
 
 from typing import Union
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from . import models
@@ -13,6 +14,21 @@ from .db import engine, SessionLocal
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 # Dependency
